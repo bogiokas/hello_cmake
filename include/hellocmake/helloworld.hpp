7bd78f607baf56b657f50c9998163ac7a2887e07
bogiokas <mpogiokas@hotmail.com>
2023-03-24 16:09:01 +0100

    hello add_library

---
+++ include/hellocmake/helloworld.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include<string>
+
+void helloworld(const std::string& name);
---
+++ src/helloworld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "hellocmake/helloworld.hpp"
+
+void helloworld(const std::string& name) {
+    std::cout<<"Hello "<<name<<"!"<<std::endl;
+}
+
